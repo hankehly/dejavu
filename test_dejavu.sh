33c4b354fdfb2ce06222eb4aa378df29c34707b4
hankehly <henry.ehly@gmail.com>
2023-10-30 16:16:09 -0400

    Update args in test script

--- test_dejavu.sh
+++ test_dejavu.sh
@@ -11,15 +11,15 @@ rm -rf ./results ./temp_audio
 python dejavu.py -f ./mp3/ mp3
 
 ##########
-# Run a test suite on the ./mp3 folder by extracting 1, 2, 3, 4, and 5 
-# second clips sampled randomly from within each song 8 seconds 
-# away from start or end, sampling with random seed = 42, and finally 
+# Run a test suite on the ./mp3 folder by extracting 1, 2, 3, 4, and 5
+# second clips sampled randomly from within each song 8 seconds
+# away from start or end, sampling with random seed = 42, and finally
 # store results in ./results and log to dejavu-test.log
 python run_tests.py \
-	--secs 5 \
-	--temp ./temp_audio \
-	--log-file ./results/dejavu-test.log \
-	--padding 8 \
-	--seed 42 \
-	--results ./results \
+	-sec 5 \
+	-temp ./temp_audio \
+	-lf ./results/dejavu-test.log \
+	-pad 8 \
+	-sd 42 \
+	-res ./results \
 	./mp3
